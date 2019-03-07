3966f1abfbcac0bb4849b2e4bac4cadba427281b
Thierry.Duriez <thierry.duriez@hotmail.fr>
2019-03-07 07:13:17 +0100

    Exemple

--- MainProjectExample/Program.cs
+++ MainProjectExample/Program.cs
@@ -16,10 +16,69 @@ namespace MainProjectExample
 
         static void Main(string[] args)
         {
+            //For Insert one row
+            DTO_Product insertRow = new DTO_Product()
+            {
+                IsNew = true,
+                ProductId = -1,
+                RegionId = 1,
+                ProductCode = "ProdCode",
+                ProductLabel = "My Product Label",
+                IsActive = true,
+                ProductDateCreation = DateTime.Now
+            };
+
+            DataBaseWithDapper<DTO_Product> dbwdInsert = new DataBaseWithDapper<DTO_Product>();
+            int id = dbwdInsert.Repository_Bdd.Insert(insertRow);
+
+
+            //For Update one row
+            DTO_Product updateRow = new DTO_Product()
+            {
+                IsNew = false,
+                ProductId = 1,
+                RegionId = 1,
+                ProductCode = "NewProdCode",
+                ProductLabel = "New Product Label",
+                IsActive = true,
+                ProductDateCreation = DateTime.Now
+            };
+
+            DataBaseWithDapper<DTO_Product> dbwdUpdate = new DataBaseWithDapper<DTO_Product>();
+            bool updateOK = dbwdInsert.Repository_Bdd.Update(updateRow);
+
+
+            //For Delete one row
+            DTO_Product deleteRow = new DTO_Product()
+            {
+                IsNew = false,
+                ProductId = 1,
+            };
+            DataBaseWithDapper<DTO_Product> dbwdDelete = new DataBaseWithDapper<DTO_Product>();
+            bool DeleteOK = dbwdInsert.Repository_Bdd.Delete(deleteRow);
+
+
+            //For Extract all rows
             List<DTO_Product> lstAllRows = null;
-            DataBaseWithDapper<DTO_Product> dbwd = new DataBaseWithDapper<DTO_Product>();            
-            lstAllRows = dbwd.Repository_Bdd.GetAll().ToList();
-        
+            DataBaseWithDapper<DTO_Product> dbwdAllSelect = new DataBaseWithDapper<DTO_Product>();            
+            lstAllRows = dbwdAllSelect.Repository_Bdd.GetAll().ToList();
+
+
+            //For Extract all rows WithCriteria
+            List<DTO_Product> lstAllRowsWithCriteria = null;
+            DataBaseWithDapper<DTO_Product> dbwdAllSelectWithCriteria = new DataBaseWithDapper<DTO_Product>();
+            lstAllRowsWithCriteria = dbwdAllSelectWithCriteria.Repository_Bdd.GetAllWithCriteria("ProductLabel like '%label%'").ToList();
+
+
+            //For Extract one row
+            DTO_Product selectOneRow = new DTO_Product()
+            {
+                IsNew = false,
+                ProductId = 1,
+            };
+            DataBaseWithDapper<DTO_Product> dbwdOneSelect = new DataBaseWithDapper<DTO_Product>();
+            selectOneRow = dbwdAllSelect.Repository_Bdd.GetSingleByPK(selectOneRow);
+
         }
 
     }
